4cd6cc0e3fd291dfcd6a445a844f033be81fe22e
Your Name <you@example.com>
2021-12-18 16:37:40 -0800

    refactor done.  New tests pass.  Old tests don't

--- src/fiddle/__init__.py
+++ src/fiddle/__init__.py
@@ -1,6 +1,25 @@
 import os
 import pkg_resources
 
+from .Builder import BuildSpec
+from .MakeBuilder import MakeBuilder
+from .LocalRunner import LocalRunner
+from .Runner import Invocation, Runnable
+from .Data import InvocationResultsList
+
+
+def build_and_run(source_file, build_parameters, function, arguments):
+    build = BuildSpec(source_file, build_parameters=build_parameters)
+
+    invocations_spec = Runnable(function=function, arguments=arguments)
+    
+    executable = MakeBuilder(build, verbose=True).build()
+
+    invocation = Invocation(executable, invocations_spec)
+
+    result = LocalRunner(invocation).run()
+    return result
+
 def setup_ld_path():
     PACKAGE_DATA_PATH = pkg_resources.resource_filename('fiddle', 'resources/')
     ld_paths = os.environ["LD_LIBRARY_PATH"].split(":");
