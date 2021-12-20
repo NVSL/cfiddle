f0cb813693fde2c93053336ce0df46c1248e946d
Your Name <you@example.com>
2021-12-19 20:23:34 -0800

    pop/pop for configurations

--- src/fiddle/config.py
+++ src/fiddle/config.py
@@ -1,3 +1,5 @@
+import copy
+
 from .Data import InvocationResultsList
 from .Builder import Executable, ExecutableDescription
 from .MakeBuilder import MakeBuilder
@@ -6,7 +8,9 @@ from .LocalRunner import LocalRunner
 from .CProtoParser import CProtoParser
 from .source import FullyInstrumentedExecutable
 
-fiddle_config = dict(Executable_type=FullyInstrumentedExecutable,
+from contextlib import contextmanager
+
+default_config = dict(Executable_type=FullyInstrumentedExecutable,
                      InvocationResult_type=InvocationResult,
                      Builder_type=MakeBuilder,
                      Runner_type=LocalRunner,
@@ -16,10 +20,36 @@ fiddle_config = dict(Executable_type=FullyInstrumentedExecutable,
                      ProtoParse_type=CProtoParser)
 
 
+fiddle_config_stack = []
+fiddle_config_stack.append(default_config)
 
-def set_config(k,v):
-    fiddle_config[k] = v
-
-def get_config(k):
-    return fiddle_config[k]
     
+def set_config(k,v):
+    global fiddle_config_stack
+    fiddle_config_stack[-1][k] = v
+
+    
+def get_config(k):
+    global fiddle_config_stack
+    return fiddle_config_stack[-1][k]
+
+@contextmanager
+def fiddle_config(**kwargs):
+    push_config()
+    [set_config(k,v) for k,v in kwargs.items()]
+    try:
+        yield
+    finally:
+        pop_config()
+        
+
+def push_config():
+    global fiddle_config_stack
+    fiddle_config_stack.append(copy.deepcopy(fiddle_config_stack[-1]))
+
+    
+def pop_config():
+    global fiddle_config_stack
+    if len(fiddle_config_stack) == 1:
+        raise ValueError("Poping the fiddle configuration stack would leave it empty.")
+    fiddle_config_stack = fiddle_config_stack[:-1]
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,16 +1,35 @@
 from fiddle import *
 from fiddle.Builder import Executable
+from fiddle.config import *
 import fiddle
+import pytest
 
 def test_configuration():
 
     assert isinstance(build_one("test_src/test.cpp"), Executable)
 
-    class MyExecutable(Executable):
+    push_config()
+    set_config("Executable_type", MyExecutable)
+    assert isinstance(build_one("test_src/test.cpp"), MyExecutable)
+    pop_config()
+    assert not isinstance(build_one("test_src/test.cpp"), MyExecutable)
+
+
+def test_push_pop():
+
+    with pytest.raises(ValueError):
+        pop_config()
+
+    with pytest.raises(ValueError):
+        push_config()
+        pop_config()
+        pop_config()
+    
+    with fiddle_config(Executable_type=MyExecutable):
+        assert isinstance(build_one("test_src/test.cpp"), MyExecutable)
+    assert not isinstance(build_one("test_src/test.cpp"), MyExecutable)
+        
+
+class MyExecutable(Executable):
         pass
 
-    old = fiddle.get_config("Executable_type")
-    fiddle.set_config("Executable_type", MyExecutable)
-    assert isinstance(build_one("test_src/test.cpp"), MyExecutable)
-    fiddle.set_config("Executable_type", old)
-    
