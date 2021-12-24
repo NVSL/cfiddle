e25f18d30103d67dd00ec93aa6d70974b7c8be85
Your Name <you@example.com>
2021-12-24 00:32:16 -0800

    remove stale code

--- tests/test_src/test.cpp
+++ tests/test_src/test.cpp
@@ -5,8 +5,8 @@
 // Don't delete these.
 
 #include<iostream>
-#include"collector.hpp"
 #include<cstring>
+#include"fiddle.hpp"
 
 template<class T>
 void go(T v) {
@@ -45,32 +45,6 @@ extern "C"
 int four() {
 	return 4;
 }
-
-extern "C"
-void print_something(int a, int b, int c) {
-
-	std::cerr << a << " " << b << "\n";
-	std::cout << a<< " " << b << "\n";
-
-	Dataset ds;
-	ds.set("hello", 8);
-	ds.set("hello2", std::string("aoe"));
-	ds.set("hello3", strdup("AOE"));
-	ds.set("hello4", "AOE");
-
-	// C++ CSV output
-	
-	//https://gist.github.com/rudolfovich/f250900f1a833e715260a66c87369d15
-	std::cout << ds.get_datum("hello") << "\n";
-	std::cout << ds.get_datum("hello2") << "\n";
-	std::cout << ds.get_datum("hello3") << "\n";
-	std::cout << ds.get_datum("hello4") << "\n";
-
-	go(4);
-	
-	std::cout.flush();
-}
-
 
 // don't delete this. it's for a test
 //HERE
