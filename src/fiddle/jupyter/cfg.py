fbd5ccb7650f4d13a4718db1bbd7f2c720be36d8
Your Name <you@example.com>
2021-12-26 20:39:18 -0800

    update figures

--- src/fiddle/jupyter/cfg.py
+++
@@ -1,8 +0,0 @@
-from cfg.cfg import do_cfg
-
-def cfg(built_result, symbol=None, *argc, **kwargs):
-    if symbol is None:
-        symbol = built_result.get_default_function_name()
-    if not symbol:
-        raise ValueError("Couldn't find default function in build {build_result}")
-    return do_cfg(built_result.lib, symbol=symbol, *argc, **kwargs)
