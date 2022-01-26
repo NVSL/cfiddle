5122273f7d8b5e7144fcff8f53834839b55c3f7d
Steven Swanson <swanson@cs.ucsd.edu>
2022-01-26 14:20:59 -0800

    build redare from source.

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -6,9 +6,19 @@ apt-get install -y make less emacs-nox gcc make g++ cmake gdb build-essential gr
 #gcc-8 g++-8 libhdf5-dev uuid-runtime  openssh-client time  default-jdk
 
 ##### Redare2 (for CFG generation)
-curl -L https://github.com/radareorg/radare2/releases/download/5.3.1/radare2-dev_5.3.1_amd64.deb -o /tmp/radare2-dev_5.3.1_amd64.deb
-curl -L https://github.com/radareorg/radare2/releases/download/5.3.1/radare2_5.3.1_amd64.deb -o /tmp/radare2_5.3.1_amd64.deb
-apt install /tmp/radare2_5.3.1_amd64.deb  /tmp/radare2-dev_5.3.1_amd64.deb
+#apt-get install -y radare2  # doesn't work because the version is old
+#if [ x"$(uname -a)" = x"aarch64" ]; then
+#    git clone https://github.com/radareorg/radare2
+#    radare2/sys/install.sh
+#else
+    (cd tmp; git clone -b 5.5.4 https://github.com/radareorg/radare2
+     radare2/sys/install.sh --install
+    )
+#    curl -L https://github.com/radareorg/radare2/releases/download/5.3.1/radare2-dev_5.3.1_amd64.deb -o /tmp/radare2-dev_5.3.1_amd64.deb
+#    curl -L https://github.com/radareorg/radare2/releases/download/5.3.1/radare2_5.3.1_amd64.deb -o /tmp/radare2_5.3.1_amd64.deb
+#    apt install /tmp/radare2_5.3.1_amd64.deb  /tmp/radare2-dev_5.3.1_amd64.deb
+#fi
+
 
 ##### Go (for go support)
 apt-get install -y golang-go
