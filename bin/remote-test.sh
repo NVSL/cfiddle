f4157b9dadd508b6a8e3eb2d18a0e243095067fb
Steven Swanson <swanson@cs.ucsd.edu>
2022-02-04 13:35:58 -0800

    test

--- bin/remote-test.sh
+++ bin/remote-test.sh
@@ -2,13 +2,6 @@
 
 set -ex
 
-branch=$1
-shift
-
-rm -rf .remote_test
-
-git clone  http://github.com/NVSL/fiddle.git .remote_test
-cd .remote_test
 python3 -m venv venv
 . venv/bin/activate
 pip install .
