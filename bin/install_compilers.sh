10b94fd5325a6cc1b2c4ab2e37d210c6a02c5783
Steven Swanson <swanson@cs.ucsd.edu>
2022-02-04 21:46:22 -0800

    generalized cross-compiler installation. fixed

--- bin/install_compilers.sh
+++ bin/install_compilers.sh
@@ -3,13 +3,17 @@
 
 
 # GCC cross compilers
-while read prefix libpfm4_arch; do
+while read prefix libpfm4_arch extra_packages; do
     (
 	apt-get install -y g++-$prefix gcc-$prefix binutils-$prefix || exit 0 # it's ok to fail here.  we just skip it
+
+	if [ x"$extra_packages" != x ]; then
+	    apt-get install -y $extra_packages
+	fi
 	
 	export CXX=$prefix-g++
 	export CC=$prefix-gcc
-	
+
 	cd /tmp
 	rm -rf libpfm4;
 	git clone http://github.com/wcohen/libpfm4.git
@@ -19,7 +23,7 @@ while read prefix libpfm4_arch; do
 done <<EOF
 powerpc-linux-gnu powerpc 
 x86-64-linux-gnu x86_64 
-arm-linux-gnueabi arm 
+arm-linux-gnueabi arm g++-8-arm-linux-gnueabi gcc-8-arm-linux-gnueabi
 EOF
 
 ##### Go 
