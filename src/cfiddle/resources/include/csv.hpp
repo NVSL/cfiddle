ee819c4dec0ea701ee8c1aa00188f447715eb4e6
Your Name <you@example.com>
2022-11-24 00:46:22 -0800

    Fix CSV escaping.  Tests for same.  Fixes #14

--- src/cfiddle/resources/include/csv.hpp
+++ src/cfiddle/resources/include/csv.hpp
@@ -79,14 +79,13 @@ private:
 			{
 				is_first_ = false;
 			}
-			fs_ << val;
+			fs_ << "\"" << val << "\"";
 			return *this;
 		}
 
 	std::string escape(const std::string & val)
 		{
 			std::ostringstream result;
-			result << '"';
 			std::string::size_type to, from = 0u, len = val.length();
 			while (from < len &&
 			       std::string::npos != (to = val.find_first_of(special_chars_, from)))
@@ -94,7 +93,7 @@ private:
 				result << val.substr(from, to - from) << escape_seq_ << val[to];
 				from = to + 1;
 			}
-			result << val.substr(from) << '"';
+			result << val.substr(from);
 			return result.str();
 		}
 };
