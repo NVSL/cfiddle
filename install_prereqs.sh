4fac71a402c38c9a4647734a34199ed4c40aac35
Your Name <you@example.com>
2022-11-25 14:29:14 -0800

    build and install perf for current kernel

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -22,6 +22,18 @@ apt-get install -y gcc-8 g++-8 || true # this fails on circleci for some reason
 # we do this instead of apt-get because showevtinfo is very useful and it's not installed by default.
 (cd /tmp; rm -rf libpfm4; echo yes | git clone http://github.com/wcohen/libpfm4.git && cd libpfm4 && make && make install && cp examples/showevtinfo /usr/local/bin)
 
+##### perf
+apt-get -y install flex bison && apt-get clean -y
+# for some reason the .0 revisions don't exist on kernel.org.
+kernel_version=$(uname -r | perl -ne '($m, $n, $r) = /(\d+)\.(\d+)\.(\d+)/; if ($r==0) {$r=1} print "$m.$n.$r"')
+(rm -rf /tmp/perf;
+ mkdir /tmp/perf;
+ cd /tmp/perf;
+ curl -L https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$kernel_version.tar.xz -o linux-$kernel_version.tar.xz ;
+ tar xf linux-$kernel_version.tar.xz;
+ cd linux-$kernel_version;
+ PYTHON=python3 make -C tools/perf install  DESTDIR=/usr/local)
+
 if [ x"$CFIDDLE_INSTALL_CROSS_COMPILERS" = x"yes" ]; then
     bin/install_compilers.sh
 fi
