8d08d63a0c518595e3c17780bb9299a681253a95
Your Name <you@example.com>
2022-12-12 00:09:10 -0800

    more robust installation of perf.  cleanup some tests

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -24,33 +24,7 @@ apt-get install -y gcc-8 g++-8 || true # this fails on circleci for some reason
 
 
 ##### perf
-if ! perf --version; then
-    apt-get update --fix-missing -y
-    apt-get -y install flex bison python3-pip && apt-get clean -y
-    python3 -m pip install python-config
-    
-    pushd .
-    if apt-get install linux-source; then
-	kernel_version=$(uname -r | perl -ne '($m, $n, $r) = /(\d+)\.(\d+)\.(\d+)/; print "$m.$n.$r"')
-	cd /usr/src/linux-source-$kernel_version
-	tar xf *.bz2
-	cd linux-source-$kernel_version
-    else
-	# for some reason the .0 revisions don't exist on kernel.org.
-	kernel_version=$(uname -r | perl -ne '($m, $n, $r) = /(\d+)\.(\d+)\.(\d+)/; if ($r==0) {$r=1} print "$m.$n.$r"')
-	rm -rf /tmp/perf;
-	mkdir /tmp/perf;
-	pushd /tmp/perf;
-	curl -L https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$kernel_version.tar.xz -o linux-$kernel_version.tar.xz ;
-	tar xf linux-$kernel_version.tar.xz;
-    fi
-    PYTHON=python3 make -C tools/perf install  DESTDIR=/usr/local
-    
-    rm -rf /tmp/perf
-
-    popd
-fi
-
+bin/install_perf.sh
 
 if [ x"$CFIDDLE_INSTALL_CROSS_COMPILERS" = x"yes" ]; then
     bin/install_compilers.sh
