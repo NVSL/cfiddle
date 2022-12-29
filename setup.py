aeb3d220b23b1da0d955d470a6aedbe8617a0732
Your Name <you@example.com>
2022-12-29 15:18:41 -0800

    update python versions

--- setup.py
+++ setup.py
@@ -70,11 +70,11 @@ setup(
         'Framework :: Jupyter',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         'Programming Language :: Python :: 3', 
-#        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     url="https://github.com/NVSL/cfiddle",
     author="Steven Swanson",
