e8b9a345ce0add1cb53e0d2b3c1d164847eacb89
Steven Swanson <swanson@cs.ucsd.edu>
2023-07-20 11:26:02 -0700

    provide more debugging output for install_prereqs.sh

--- bin/cfiddle_install_prereqs.sh
+++ bin/cfiddle_install_prereqs.sh
@@ -12,7 +12,7 @@ apt-get install -y gcc-8 g++-8 || true # this fails on circleci for some reason
 
 
 ##### Redare2 (for CFG generation)
-(cd /tmp;
+(set -ex; cd /tmp;
  rm -rf radare2;
  echo yes | git clone -b 5.5.4 http://github.com/radareorg/radare2
  cd radare2
@@ -22,12 +22,12 @@ apt-get install -y gcc-8 g++-8 || true # this fails on circleci for some reason
 
 
 ##### Google test
-(cd /tmp; rm -rf googletest; git clone http://github.com/google/googletest.git && cd googletest && cmake CMakeLists.txt; make install)
+(set -ex; cd /tmp; rm -rf googletest; git clone http://github.com/google/googletest.git && cd googletest && cmake CMakeLists.txt; make install)
 
 
 ##### libpfm4
 # we do this instead of apt-get because showevtinfo is very useful and it's not installed by default.
-(cd /tmp; rm -rf libpfm4; echo yes | git clone http://github.com/wcohen/libpfm4.git && cd libpfm4 && make && make install && cp examples/showevtinfo /usr/local/bin)
+(set -ex; cd /tmp; rm -rf libpfm4; echo yes | git clone http://github.com/wcohen/libpfm4.git && cd libpfm4 && make && make install && cp examples/showevtinfo /usr/local/bin)
 
 
 ##### perf
